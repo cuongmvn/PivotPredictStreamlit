9403c9676ee68449cbc0b33dcd2e113d827a2cac
cuongmvn <cuongmvn@gmail.com>
2022-06-05 20:06:31 +0200

    Streamlit frontend

--- pivotapp.py
+++ pivotapp.py
@@ -13,5 +13,5 @@ with st.expander("Details"):
 
 uploaded_file = st.file_uploader("Choose a file for prediction")
 if uploaded_file is not None:
-    res = requests.post(f"https://predictjobtitle.herokuapp.com/predict-csv", file=uploaded_file)
+    res = requests.post(f"https://predictjobtitle.herokuapp.com/predict-csv", files=uploaded_file)
     st.write(res.json())
